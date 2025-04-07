c3f3d1e0562f375e32c5a8ec26e259dfea925ad0
Criket34 <168623069+Criket34@users.noreply.github.com>
2025-04-08 07:12:23 +0900

    Add files via upload

---
+++ BMI/main.js
@@ -0,0 +1,20 @@
+var text = 'Javascriptを勉強中です。';
+console.log(text);
+var number = 3 + 5;
+console.log(number);
+var number1 = 3;
+var text1 = '5';
+console.log(number1 + number1);
+console.log(text1 + text1);
+
+var lang = 'Javascript';
+var action = 'を勉強中です。';
+console.log(lang + action);
+
+console.log(number1 + text1);
+
+var data = ["りんご", "みかん", "ぶどう", "メロン"];
+console.log(data[0]);
+console.log(data[1]);
+console.log(data[2]);
+console.log(data[3]);
